e1fba24b0fed36975b128c439e7293934b68e066
BaeMinsoo <alstn3383@gmail.com>
2022-04-18 14:02:38 +0900

    중복폴더제거

--- reviewBook/src/kh/semi/reviewBook/user/controller/LoginController.java
+++ reviewBook/src/kh/semi/reviewBook/user/controller/LoginController.java
@@ -25,7 +25,7 @@ public class LoginController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("WEB-INF/view/member/login.jsp");
+		request.getRequestDispatcher("WEB-INF/view/user/login.jsp");
 	}
 
 	/**
