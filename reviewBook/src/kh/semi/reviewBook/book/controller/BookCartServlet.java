554f95a03f26612c9e454171d9e60475165d2b6b
byeongguk93 <sensual12@naver.com>
2022-05-12 11:17:04 +0900

    bookcartservlet 재푸쉬

--- reviewBook/src/kh/semi/reviewBook/book/controller/BookCartServlet.java
+++ reviewBook/src/kh/semi/reviewBook/book/controller/BookCartServlet.java
@@ -1,6 +1,11 @@
 package kh.semi.reviewBook.book.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,57 +39,61 @@ public class BookCartServlet extends HttpServlet {
 	 */
 
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	      System.out.println("do post : /bookaddcart");
-	      String bkNostr = request.getParameter("bkNo");
-	      System.out.println("bkNo : "+bkNostr);
-	      int bkNo = 0;
-	      try {
-	         bkNo = Integer.parseInt(bkNostr);
-	      }catch(NumberFormatException e) {
-	         e.printStackTrace();
-	      }
-	      if(bkNo < 1) { 
-	         //읽어올 게시글 번호가 이상할 경우 그냥 메인으로 보냄
-	         response.sendRedirect("bookcontent?wbNo="+bkNo);
-	         //콘솔에서 확인
-	         System.out.println("bkNo < 1");
-	      }
-	      String caCountstr = request.getParameter("caCount");
-	      System.out.println("caCountstr : "+ caCountstr);
-	      int caCount = 0;
-	      try {
-	         caCount = Integer.parseInt(caCountstr);
-	      }catch(NumberFormatException e) {
-	         e.printStackTrace();
-	      }
-	      
-	      String usId = "";
-	   
-	            
-	      //로그인 정보 읽어오기
-	      UserVo vo = (UserVo)request.getSession().getAttribute("ssUserVo"); 
-	      if(vo == null) { //로그인이 되지 않은 상황 -> 로그인 해야만 글 작성 가능 ->로그인으로 이동
-	      response.sendRedirect("login");
-	      return; 
-	      } else {
-	      usId = vo.getUsId();
-	      System.out.println(usId);
-	      }
-	      
-	      CartVo cvo = new CartVo();
-	      cvo.setBkNo(bkNo);
-	      cvo.setCaCount(caCount);
-	      cvo.setUsId(usId);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("do post : /bookaddcart");
+		String bkNostr = request.getParameter("bkNo");
+		System.out.println("bkNo : " + bkNostr);
+		int bkNo = Integer.parseInt(bkNostr);
+		try {
+			bkNo = Integer.parseInt(bkNostr);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		if (bkNo < 1) {
+			// 읽어올 책번호가 이상할 경우 그냥 도서메인으로 보냄
+			response.sendRedirect("bookcontent?bkNo=" + bkNo);
+			// 콘솔에서 확인
+			System.out.println("bkNo < 1");
+		}
+		String caCountstr = request.getParameter("caCount");
+		System.out.println("caCountstr : " + caCountstr);
+		int caCount = 0;
+		try {
+			caCount = Integer.parseInt(caCountstr);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
 
-	       int result = new BookService().insertCart(cvo);
-	       if(result<1) {//댓글 입력에 실패한 경우에도 메인으로 보냄
-	          response.sendRedirect("bookcontent?wbNo="+bkNo);
-	          System.out.println("실패");
-	       }else {
-	          response.sendRedirect("cartlist");
-	          System.out.println("성공");
-	       }
+
+		// 로그인 정보 읽어오기
+		String usId = "";
+		UserVo vo = (UserVo) request.getSession().getAttribute("ssUserVo");
+		if (vo == null) { // 로그인이 되지 않은 상황 -> 로그인 해야만 글 작성 가능 ->로그인으로 이동
+			response.sendRedirect("login");
+			return;
+		} else {
+			usId = vo.getUsId();
+			System.out.println(usId);
+		}
+
+		CartVo cvo = new CartVo();
+		cvo.setBkNo(bkNo);
+		cvo.setCaCount(caCount);
+		cvo.setUsId(usId);
+
+		int result = new BookService().insertCart(cvo);
+		if (result < 1) {// 장바구니 넣기 실패
+			System.out.println("실패");
+			PrintWriter out = response.getWriter();
+			out.print("<script language='javascript'>");
+			out.print("alert('이미 장바구니에 존재하는 상품입니다. 수량 변경은 장바구니에서 가능합니다.'); location.href='" + request.getContextPath() + "/cartlist'");
+			out.print("</script>");
+			out.flush();
+		} else {
+			response.sendRedirect("cartlist");
+			System.out.println("성공");
+		}
 	}
 
 }
